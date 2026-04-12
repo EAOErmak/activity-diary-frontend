2eb9947b765b3d8a3d5b9635f32c852cfd6141ee
EAOErmak <zrmek.aydarbekov@gmail.com>
2026-04-13 01:48:34 +0500

    added fallback

--- src/api/http/axiosInstance.ts
+++ src/api/http/axiosInstance.ts
@@ -23,7 +23,7 @@ type RetriableRequestConfig = AxiosRequestConfig & {
 // ======================================================
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:18080",
   headers: {
     "Content-Type": "application/json",
   },
